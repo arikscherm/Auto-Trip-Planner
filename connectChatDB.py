d28ae692239b7c71d6fe275435187bb25d494a87
arikscherm <arikscherm@gmail.com>
2022-10-06 17:38:49 -0600

    Added iMessage DB Connector and Driver

---
+++ connectChatDB.py
@@ -0,0 +1,22 @@
+import sqlite3
+import os
+
+
+def open_db():
+    path = "/Users/<Home Directory>/Library/Messages/"
+    os.chdir(path)
+    con = sqlite3.connect('chat.db')
+    return con
+
+def execute_query():
+    con = open_db()
+    cur = con.cursor()
+    query_result = cur.execute(
+    "SELECT m.rowid, h.id, m.text, datetime((m.date / 1000000000) + 978307200, 'unixepoch', 'localtime') as TextDate, m.is_from_me FROM message as m, handle as h on m.handle_id = h.rowid WHERE m.text like '..%' ORDER BY DATE DESC LIMIT 1"
+    )
+    query_results = tuple(query_result.fetchone())
+    con.close()
+    os.chdir("<Directory of Project>")
+    return query_results
+
+
---
+++ main.py
@@ -0,0 +1,50 @@
+import geocode.geocode as geocode
+import connectChatDB
+import time
+from datetime import datetime
+import sendWeather
+import sendCoordinates
+import sendBeta
+import sendCamps
+
+
+
+def check_for_new_message(time_of_last_message):
+    new_message = False
+    while(new_message == False):
+        new_message_data = connectChatDB.execute_query()
+        new_message_date = datetime.strptime(new_message_data[3],'%Y-%m-%d %H:%M:%S')
+        if(new_message_date > time_of_last_message):
+            new_message = True
+            return new_message_data
+        else:
+            print("Nothing new")
+            time.sleep(10)
+    
+
+def run():
+    time_of_last_message = datetime.now()
+    message_text = ""
+    while(message_text != "end"):
+        new_message_data = check_for_new_message(time_of_last_message)
+        phone_number = str(new_message_data[1])
+        message_text = new_message_data[2].replace('.','')
+        time_of_last_message = datetime.strptime(new_message_data[3],'%Y-%m-%d %H:%M:%S')
+
+        
+
+        coordinates = sendCoordinates.getCoordinates(message_text)
+        dict_coordinates = sendCoordinates.get_dict_coordinates(message_text)
+
+        
+        sendCoordinates.sendCoordinates(phone_number,coordinates)
+        sendWeather.send_weather(phone_number,dict_coordinates)
+        sendBeta.send_beta(phone_number,message_text)
+        sendCamps.send_campsites(phone_number,dict_coordinates)
+        
+
+
+
+
+
+run()
