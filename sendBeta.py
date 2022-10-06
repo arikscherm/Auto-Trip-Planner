e9beee2b9a32a502e039bb6400a6437e5e1483cb
arikscherm <101311023+arikscherm@users.noreply.github.com>
2022-10-06 17:34:01 -0600

    Update sendBeta.py

--- sendBeta.py
+++ sendBeta.py
@@ -5,8 +5,8 @@ import os
 
 def send_beta(phone_number,message_text):
     beta.screenshot_beta.get_beta(message_text)
-    beta_screenshot = "/Users/arischermer/Desktop/code/AutoTripPlanner/beta/beta.png"
-    cover_image_screenshot = "/Users/arischermer/Desktop/code/AutoTripPlanner/beta/beta/cover_image.png"
+    beta_screenshot = "/<Directory of Project>/beta/beta.png"
+    cover_image_screenshot = "/<Directory of Project>/beta/cover_image.png"
     callAppleScript.send_message(phone_number,beta_screenshot)
     callAppleScript.send_message(phone_number,cover_image_screenshot)
     return "Successfully sent beta"
