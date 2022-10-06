ea58dd134608ff656ddfb518087141ffcadd5bd9
arikscherm <arikscherm@gmail.com>
2022-10-06 17:28:11 -0600

    Updated Weather PAPI

--- weather/weather_papi.py
+++ weather/weather_papi.py
@@ -1,11 +1,12 @@
 from weather import weather_sapi
 import pprint
 
-def get_periods():
-    response = weather_sapi.get_sapi_response()
+def get_periods(coordinates):
+    print("Now inside the papi", coordinates)
+    response = weather_sapi.get_sapi_response(coordinates)
+    print(response)
     periods = response["properties"]["periods"]
     #pprint.pprint(periods)
     return periods[0]
 
 
-#TODO: reduce what the weather papi returns
